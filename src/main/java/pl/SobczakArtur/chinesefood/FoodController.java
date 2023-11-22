cf9f4e0be47b49a6479757826cc51c4617a98535
Artur Sobczak <sobczak.artur88@gmail.com>
2023-11-22 23:45:19 +0100

    First method

--- src/main/java/pl/SobczakArtur/chinesefood/FoodController.java
+++ src/main/java/pl/SobczakArtur/chinesefood/FoodController.java
@@ -1,13 +1,24 @@
 package pl.SobczakArtur.chinesefood;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FoodController {
 
+    @Autowired
+    FoodRepository foodRepository;
+
     @GetMapping("/test")
     public String test(){
         return "Działa";
+    }
+
+    @GetMapping("/dishes")
+    public List<Food> getAll(){
+        return foodRepository.getAll();
     }
 }
--- src/main/java/pl/SobczakArtur/chinesefood/FoodRepository.java
+++ src/main/java/pl/SobczakArtur/chinesefood/FoodRepository.java
@@ -1,10 +1,21 @@
 package pl.SobczakArtur.chinesefood;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class FoodRepository {
 
-    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Food> getAll(){
+        return jdbcTemplate.query("SELECT * FROM foodmenu",
+                BeanPropertyRowMapper.newInstance(Food.class));
+    }
 
 }
